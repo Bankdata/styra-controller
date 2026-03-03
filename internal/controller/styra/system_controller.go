0239d85a8b678183b44ca6046f0819bd7dc7ebf6
ChrBlaesBD <cbs@bankdata.dk>
2026-03-03 11:38:41 +0100

    for jacob- works

--- internal/controller/styra/system_controller.go
+++ internal/controller/styra/system_controller.go
@@ -857,7 +857,7 @@ func bundleRevision(system *v1beta1.System, requirements []ocp.Requirement) stri
 func requirementRevisionExpression(requirement ocp.Requirement) string {
 	if requirement.RevisionHash && requirement.RevisionCommit {
 		return fmt.Sprintf(
-			`{input.sources[\"%s\"].git.commit}-{input.sources[\"%s\"].sql.hash}`,
+			"commit:{input.sources[\"%s\"].git.commit}-data:{input.sources[\"%s\"].sql.hash}",
 			requirement.Source,
 			requirement.Source,
 		)
@@ -865,13 +865,13 @@ func requirementRevisionExpression(requirement ocp.Requirement) string {
 
 	if requirement.RevisionHash {
 		return fmt.Sprintf(
-			`{input.sources[\"%s\"].sql.hash}`,
+			"data:{input.sources[\"%s\"].sql.hash}",
 			requirement.Source,
 		)
 	}
 
 	return fmt.Sprintf(
-		`{input.sources[\"%s\"].git.commit}`,
+		"commit:{input.sources[\"%s\"].git.commit}",
 		requirement.Source,
 	)
 }
